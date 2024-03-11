7e0ce036c80388b5b95b6bc6bf4a5dca1284e7fd
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-11 01:22:09 +0000

    chore: add licenses

--- contracts/UniswapFactoryHelper.sol
+++ contracts/UniswapFactoryHelper.sol
@@ -1,5 +1,5 @@
-
-pragma solidity =0.5.16;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.5.16;
 
 interface IUniswapV2Factory {
     function getPair(address tokenA, address tokenB) external view returns (address pair);
