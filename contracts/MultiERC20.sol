980ff57240d04d2a0cf2057b299cfd389eec6d35
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-17 18:02:50 +0000

    feat: MultiERC20 decimalses method

--- contracts/MultiERC20.sol
+++ contracts/MultiERC20.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 interface IERC20 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
+    function decinals() external view returns (uint8);
     function balanceOf(address) external view returns (uint);
     function totalSupply() external view returns (uint);
 }
@@ -32,6 +33,14 @@ contract MultiERC20 {
             supplys[i] = tokens[i].totalSupply();
         }
         return supplys;
+    }
+    function decimalses(IERC20[] memory tokens) public view returns (uint8[] memory) {
+        uint tokensLength = tokens.length;
+        string[] memory _decimals = new string[](tokensLength);
+        for (uint i = 0; i < tokensLength; i++) {
+            _decimals[i] = tokens[i].decimals();
+        }
+        return _decimals;
     }
     function names(IERC20[] memory tokens) public view returns (string[] memory) {
         uint tokensLength = tokens.length;
